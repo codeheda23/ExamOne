b169837b9ca2c90e9d0ed71d6c9a2111ed13bff9
codeheda23@outlook.com <codeheda23@outlook.com>
2025-08-11 11:04:09 +0800

    Feat: add xml and json api response models

---
+++ WeatherAPIClient/Model/WeatherAPIJsonResponse.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace TakeHomeTasks.WeatherAPIClient.Model.Json
+{
+    public class WeatherAPIJsonResponse
+    {
+        public string Name { get; set; }
+        public MainData Main { get; set; }
+        public Coord Coord { get; set; }
+        public Sys Sys { get; set; }
+    }
+
+    public class Coord
+    {
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+    }
+
+    public class MainData
+    {
+        public double Temp { get; set; }
+        public double Feels_Like { get; set; }
+    }
+    public class Sys
+    {
+        public string Country { get; set; }
+    }
+
+}
+
---
+++ WeatherAPIClient/Model/WeatherAPIXmlResponse.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace TakeHomeTasks.WeatherAPIClient.Model.Xml
+{
+    [XmlRoot("current")]
+    public class WeatherAPIXmlResponse
+    {
+        [XmlElement("city")]
+        public City City { get; set; }
+
+        [XmlElement("temperature")]
+        public Temperature Temperature { get; set; }
+
+        [XmlElement("feels_like")]
+        public FeelsLike FeelsLike { get; set; }
+    }
+
+    public class City
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlElement("coord")]
+        public Coord Coord { get; set; }// Coord();
+
+        [XmlElement("country")]
+        public string Country { get; set; }
+
+    }
+
+    public class Coord
+    {
+        [XmlAttribute("lon")]
+        public double Lon { get; set; }
+
+        [XmlAttribute("lat")]
+        public double Lat { get; set; }
+    }
+
+    public class Temperature
+    {
+        [XmlAttribute("value")]
+        public double Value { get; set; }
+
+    }
+
+    public class FeelsLike
+    {
+        [XmlAttribute("value")]
+        public double Value { get; set; }
+    }
+}
+
+  
